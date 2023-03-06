ae5a2a30fbc186176dd66d5f6285ea57f7272aa4
Gourav Pundir <107459689+Gouravpundir@users.noreply.github.com>
2023-03-06 18:12:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ const app = express();
 const route = require("./routes/route");
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({origin:https://altius-hospital.vercel.app/}));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "/*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 
 app.use(express.json());
 
