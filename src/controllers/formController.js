62fcc2b726f98c78168c5a190f009300487b9e04
Gourav Pundir <107459689+Gouravpundir@users.noreply.github.com>
2023-03-06 19:23:22 +0530

    Update formController.js

--- src/controllers/formController.js
+++ src/controllers/formController.js
@@ -14,57 +14,57 @@ const createForm = async (req, res) => {
     //     .status(400)
     //     .send({ status: false, msg: "Request body can't be empty" });
     // }
-    // const { name, email, phone, date, time } = requestBody;
-    // if (!name) {
-    //   return res
-    //     .status(400)
-    //     .send({
-    //       status: false,
-    //       msg: "Name is required and must only contain letters",
-    //     });
-    // }
-    // if (!email) {
-    //   return res.status(400).send({ status: false, msg: "Email is required" });
-    // }
-    // if (!phone) {
-    //   return res
-    //     .status(400)
-    //     .send({ status: false, msg: "Phone number is required" });
-    // }
-    // if (!date) {
-    //   return res.status(400).send({ status: false, msg: "Date is required" });
-    // }
-    // if (!time) {
-    //   return res.status(400).send({ status: false, msg: "Time is required" });
-    // }
+    const { name, email, phone, date, time } = requestBody;
+    if (!name) {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          msg: "Name is required and must only contain letters",
+        });
+    }
+    if (!email) {
+      return res.status(400).send({ status: false, msg: "Email is required" });
+    }
+    if (!phone) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "Phone number is required" });
+    }
+    if (!date) {
+      return res.status(400).send({ status: false, msg: "Date is required" });
+    }
+    if (!time) {
+      return res.status(400).send({ status: false, msg: "Time is required" });
+    }
 
-    // if (!validateEmail(email)) {
-    //   return res
-    //     .status(400)
-    //     .send({ status: false, msg: "Please provide a valid email" });
-    // }
+    if (!validateEmail(email)) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "Please provide a valid email" });
+    }
 
-    // if (!validateMobile(phone)) {
-    //   return res
-    //     .status(400)
-    //     .send({ status: false, msg: "Please provide a valid phone number" });
-    // }
+    if (!validateMobile(phone)) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "Please provide a valid phone number" });
+    }
 
-    // if (!isValidDate(date)) {
-    //   return res
-    //     .status(400)
-    //     .send({
-    //       status: false,
-    //       msg: "Please provide a valid date (YYYY-MM-DD)",
-    //     });
-    // }
+    if (!isValidDate(date)) {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          msg: "Please provide a valid date (YYYY-MM-DD)",
+        });
+    }
 
-    // const existingForm = await formModel.findOne({ email });
-    // if (existingForm) {
-    //   return res
-    //     .status(400)
-    //     .send({ status: false, msg: "Email address already exists" });
-    // }
+    const existingForm = await formModel.findOne({ email });
+    if (existingForm) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "Email address already exists" });
+    }
 
     const newForm = await formModel.create(requestBody);
     res
